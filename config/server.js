87fb48638476fa6cb7b63462e3a904da4b3b564b
Timofey Lebedev <timofey@upsidefi.com>
2023-12-07 15:09:49 +0100

    fix deploy

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ const cronTasks = require("./cron-tasks");
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
@@ -10,4 +10,5 @@ module.exports = ({ env }) => ({
     enabled: true,
     tasks: cronTasks,
   },
+  url: env('URL', 'http://localhost:1337')
 });
