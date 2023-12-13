2c0fccc15aadd96096004384c4e7b2eb72a05dda
Timofey Lebedev <timofey@upsidefi.com>
2023-12-13 21:33:56 +0100

    antichiting patch, no require, no eval

--- game-node-runner.js
+++ game-node-runner.js
@@ -1,30 +1,39 @@
+const fs = require('fs');
 function run(enginePath, clientCodePath) {
-  const gameModule = require(enginePath);
-  const clientCode = require(clientCodePath);
-  const game = gameModule.Game();
-  const initialState = game.init();
-  const steps = [initialState];
-
-  let newState = null;
-
-  while(!newState?.gameOver) {
-    const userResponse = clientCode.gameTick(
-      initialState.layout,
-      initialState.pacmanCurrentIndex,
-      initialState.ghosts
-    );
-    game.setDirection(userResponse);
-
-    newState = game.tick();
-    steps.push(newState)
-    if (newState.gameOver) {
-      console.log(newState.gameOverStatus + " " + newState.score)
-      return {
-        score: newState.score,
-        steps
-      };
+  fs.readFile(clientCodePath, 'utf8', function(error, data) {
+    if (data.includes('require') || data.includes('eval') || error) {
+      const err = new Error('require is not supported');
+      err.stack = null;
+      throw error || err;
     }
-  }
+
+    const gameModule = require(enginePath);
+    const clientCode = require(clientCodePath);
+    const game = gameModule.Game();
+    const initialState = game.init();
+    const steps = [initialState];
+  
+    let newState = null;
+  
+    while(!newState?.gameOver) {
+      const userResponse = clientCode.gameTick(
+        initialState.layout,
+        initialState.pacmanCurrentIndex,
+        initialState.ghosts
+      );
+      game.setDirection(userResponse);
+  
+      newState = game.tick();
+      steps.push(newState)
+      if (newState.gameOver) {
+        console.log(newState.gameOverStatus + " " + newState.score)
+        return {
+          score: newState.score,
+          steps
+        };
+      }
+    }
+  })
 }
 
 module.exports = run;
